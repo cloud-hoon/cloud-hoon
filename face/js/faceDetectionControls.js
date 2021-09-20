da7589b5fbd12db21114d9802dd026717e5c4d4e
cloud-hoon <bnxx6778@gmail.com>
2021-09-20 16:51:47 +0800

    Add files via upload

--- face/js/faceDetectionControls.js
+++ face/js/faceDetectionControls.js
@@ -88,7 +88,7 @@ async function changeFaceDetector(detector) {
 
   $('#loader').show()
   if (!isFaceDetectionModelLoaded()) {
-    await getCurrentFaceDetectionNet().load('/')
+    await getCurrentFaceDetectionNet().load('./')
   }
 
   $(`#${detector}_controls`).show()
